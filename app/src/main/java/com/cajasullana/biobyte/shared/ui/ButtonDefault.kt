59c72277c8701c4b0a6f62ccd765c090690e2fb9
ulises2008 <leonelcovenas@17.com>
2023-11-21 01:07:00 -0500

    login bio byte

---
+++ app/src/main/java/com/cajasullana/biobyte/shared/ui/ButtonDefault.kt
@@ -0,0 +1,33 @@
+package com.cajasullana.biobyte.shared.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.cajasullana.biobyte.ui.theme.GreyDisableButton
+import com.cajasullana.biobyte.ui.theme.GreySoft
+
+@Composable
+fun ButtonDefault(text: String, color: Color, enabled: Boolean, handleClick: () -> Unit) {
+    Button(
+        enabled = enabled,
+        onClick = handleClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+            contentColor = Color.White,
+            disabledContainerColor = GreyDisableButton,
+            disabledContentColor = GreySoft,
+        ),
+        modifier = Modifier
+            .fillMaxWidth().height(48.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(text = text)
+    }
+}
