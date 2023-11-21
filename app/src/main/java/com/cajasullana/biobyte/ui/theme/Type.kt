7853810e543fccb1e65617f5c05b249509a72d8f
Rai_delgado <alexander.delgado@kushkipagos.com>
2023-11-21 00:24:16 -0500

    Add nunito font

--- app/src/main/java/com/cajasullana/biobyte/ui/theme/Type.kt
+++ app/src/main/java/com/cajasullana/biobyte/ui/theme/Type.kt
@@ -2,10 +2,18 @@ package com.cajasullana.biobyte.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cajasullana.biobyte.R
 
+val Nunito = FontFamily(
+    Font(R.font.nunito_light, FontWeight.W300),
+    Font(R.font.nunito_regular, FontWeight.W400),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_semi_bold, FontWeight.SemiBold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
